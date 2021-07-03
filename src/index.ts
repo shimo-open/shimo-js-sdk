8d31e9a2e3b96467bd830e5fb26bf0d67705cb55
yesui <yesui@shimo.im>
2021-07-03 17:28:26 +0800

    fix: export document, spreadsheet and presentation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import * as DocumentPro from './types/DocumentPro'
+import * as Document from './types/Document'
+import * as Spreadsheet from './types/Spreadsheet'
+import * as Presentation from './types/Presentation'
 
 export * from './connect'
 
 export * from './types/ShimoSDK'
 
-export { DocumentPro }
+export { DocumentPro, Document, Spreadsheet, Presentation }
