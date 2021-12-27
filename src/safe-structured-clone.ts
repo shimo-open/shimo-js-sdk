b1001da09f426101af781a8dd3b371498be32814
yesui <yesui@shimo.im>
2021-12-27 12:07:49 +0800

    fix: 修复对低版本浏览器的支持

--- src/safe-structured-clone.ts
+++ src/safe-structured-clone.ts
@@ -1,5 +1,13 @@
 import isPlainObject from 'is-plain-obj'
 
+function isInstanceOf(target: unknown, prototype: any): boolean {
+  try {
+    return target instanceof prototype
+  } catch (e) {
+    return false
+  }
+}
+
 /**
  * https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Structured_clone_algorithm
  */
@@ -11,21 +19,31 @@ export function clone(value: unknown): unknown {
     typeof value === 'undefined' ||
     value === null ||
     value instanceof Date ||
-    value instanceof ArrayBuffer ||
     value instanceof RegExp ||
-    value instanceof Map ||
-    value instanceof Set ||
-    value instanceof DataView ||
-    value instanceof File ||
-    value instanceof FileList ||
-    value instanceof Blob ||
-    value instanceof ImageBitmap ||
-    value instanceof ImageData ||
     value instanceof Boolean ||
     value instanceof String
   ) {
     return value
   }
+
+  try {
+    // 有些浏览器不支持这些对象
+    for (const proto of [
+      window.ArrayBuffer,
+      window.Map,
+      window.Set,
+      window.DataView,
+      window.File,
+      window.FileList,
+      window.Blob,
+      window.ImageBitmap,
+      window.ImageData
+    ]) {
+      if (isInstanceOf(value, proto)) {
+        return value
+      }
+    }
+  } catch (e) {}
 
   if (isPlainObject(value)) {
     const val = value as Record<string, unknown>
