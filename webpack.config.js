9d0c89af57b18d7f8b06badd9bb435548f665ff7
yesui <yesui@shimo.im>
2022-08-04 16:52:56 +0800

    fix: types

--- webpack.config.js
+++ webpack.config.js
@@ -15,11 +15,6 @@ module.exports = {
         test: /\.ts$/,
         use: 'ts-loader',
         exclude: /node_modules/
-      },
-      {
-        test: /node_modules\/shimo-js-sdk-shared\/src\/.*\.ts$/,
-        use: 'ts-loader',
-        exclude: /node_modules\/shimo-js-sdk-shared\/node_modules/
       }
     ]
   },
