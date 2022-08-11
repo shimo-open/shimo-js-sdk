fe0bc975e51ff0a4d7189bf1d12a0bfb36a9b38b
yesui <yesui@shimo.im>
2022-08-11 16:36:18 +0800

    fix: 编辑器接口参数默认类型

--- src/types/Document.ts
+++ src/types/Document.ts
@@ -32,82 +32,76 @@ export interface Editor extends BaseEditor<EventMap> {
    * 显示历史侧边栏
    * @since 22.2.1
    */
-  showHistory: (this: Editor, options: {}) => Promise<void>
+  showHistory: (options?: {}) => Promise<void>
   /**
    * 隐藏历史侧边栏
    * @since 22.2.1
    */
-  hideHistory: (this: Editor, options: {}) => Promise<void>
+  hideHistory: (options?: {}) => Promise<void>
   /**
    * 显示版本侧边栏
    * @since 22.2.1
    */
-  showRevision: (this: Editor, options: {}) => Promise<void>
+  showRevision: (options?: {}) => Promise<void>
   /**
    * 隐藏版本侧边栏
    * @since 22.2.1
    */
-  hideRevision: (this: Editor, options: {}) => Promise<void>
+  hideRevision: (options?: {}) => Promise<void>
   /**
    * 显示讨论侧边栏
    * @since 22.2.1
    */
-  showDiscussion: (this: Editor, options: {}) => Promise<void>
+  showDiscussion: (options?: {}) => Promise<void>
   /**
    * 隐藏讨论侧边栏
    * @since 22.2.1
    */
-  hideDiscussion: (this: Editor, options: {}) => Promise<void>
+  hideDiscussion: (options?: {}) => Promise<void>
   /**
    * 显示目录
    * @since 22.2.1
    */
-  showToc: (this: Editor, options: {}) => Promise<void>
+  showToc: (options?: {}) => Promise<void>
   /**
    * 隐藏目录
    * @since 22.2.1
    */
-  hideToc: (this: Editor, options: {}) => Promise<void>
+  hideToc: (options?: {}) => Promise<void>
   /**
    * 创建版本
    * @since 22.2.1
    */
-  createRevision: (
-    this: Editor,
-    options: {}
-  ) => Promise<null | DocumentErrorMessage>
+  createRevision: (options?: {}) => Promise<null | DocumentErrorMessage>
   /**
    * 进入演示模式
    * @since 22.2.1
    */
-  startDemonstration: (this: Editor, options: {}) => Promise<void>
+  startDemonstration: (options?: {}) => Promise<void>
   /**
    * 退出演示模式
    * @since 22.2.1
    */
-  endDemonstration: (this: Editor, options: {}) => Promise<void>
+  endDemonstration: (options?: {}) => Promise<void>
   /**
    * 打印
    * @since 22.2.1
    */
-  print: (this: Editor, options: {}) => Promise<void>
+  print: (options?: {}) => Promise<void>
 
   /**
    * 显示评论侧边栏
    * @since 22.6.1
    */
-  showComments: (this: Editor, options: {}) => Promise<void>
+  showComments: (options?: {}) => Promise<void>
   /**
    * 隐藏评论侧边栏
    * @since 22.6.1
    */
-  hideComments: (this: Editor, options: {}) => Promise<void>
+  hideComments: (options?: {}) => Promise<void>
   /**
    * 插入第三方应用，是否可用受版本限制。
    * @since 22.6.1
    */
-  insertExternalApp: (
-    this: Editor,
-    options: { url: string }
-  ) => Promise<null | DocumentErrorMessage>
+  insertExternalApp: (url: string) => Promise<null | DocumentErrorMessage>
 }
